00dee2d3ab75cb2960b6e634a1a0be4b3ab3d722
Andres <afguerrero@outlook.com>
2022-09-17 22:21:13 -0500

    Se agrega middleware para restringir rutas

--- server/routes/characters.router.js
+++ server/routes/characters.router.js
@@ -1,77 +1,97 @@
 const router = require('express').Router();
-
+const passport = require('passport');
 const CharacterService = require('../services/character.service');
 
 const service = new CharacterService();
 
-router.get('/', async (req, res, next) => {
-  try {
-    // eslint-disable-next-line prefer-const
-    let options = {
-      include: ['movies'],
-      where: {},
-    };
-
-    const { name, age, movies } = req.query;
-    if (name) {
-      options.where.name = name;
-    }
-
-    if (age) {
-      options.where.age = age;
-    }
-
-    if (movies) {
-      options.include[0] = {
-        association: 'movies',
-        as: 'movies',
-        where: { id: movies },
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
+    try {
+      // eslint-disable-next-line prefer-const
+      let options = {
+        include: ['movies'],
+        where: {},
       };
+
+      const { name, age, movies } = req.query;
+      if (name) {
+        options.where.name = name;
+      }
+
+      if (age) {
+        options.where.age = age;
+      }
+
+      if (movies) {
+        options.include[0] = {
+          association: 'movies',
+          as: 'movies',
+          where: { id: movies },
+        };
+      }
+
+      const allCharacters = await service.find(options);
+      res.json(allCharacters);
+    } catch (error) {
+      next(error);
     }
+  }
+);
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
+    try {
+      const { body } = req;
+      const newCharacter = await service.create(body);
+      res.json(newCharacter);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+router.get(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const character = await service.findOne(id);
+      res.json(character);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-    const allCharacters = await service.find(options);
-    res.json(allCharacters);
-  } catch (error) {
-    next(error);
+router.put(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const { body } = req;
+      const character = await service.update(id, body);
+      res.json(character);
+    } catch (error) {
+      next(error);
+    }
   }
-});
-router.post('/', async (req, res, next) => {
-  try {
-    const { body } = req;
-    const newCharacter = await service.create(body);
-    res.json(newCharacter);
-  } catch (error) {
-    next(error);
+);
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.json(id);
+    } catch (error) {
+      next(error);
+    }
   }
-});
-router.get('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const character = await service.findOne(id);
-    res.json(character);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const { body } = req;
-    const character = await service.update(id, body);
-    res.json(character);
-  } catch (error) {
-    next(error);
-  }
-});
-router.delete('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    await service.delete(id);
-    res.json(id);
-  } catch (error) {
-    next(error);
-  }
-});
+);
 
 module.exports = router;
